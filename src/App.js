1017ad2f2e4bb20993df7572f4229cdc5cd099fa
Aurelio Gonzalez <agg156231@gmail.com>
2021-11-25 21:15:29 -0700

    Agregando libreria de camara

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import Camera from 'react-html5-camera-photo';
 function App() {
+
+  function handleTakePhoto (dataUri) {
+    // Do stuff with the photo...
+    console.log('takePhoto');
+  }
+
   return (
     <div className="App">
-      
+      <Camera
+      onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
+    />
     </div>
   );
 }
